8e6a80c741fd737712ecb6468f7f2d922d2122d0
이름 <dlsxody1@naver.com>
2022-07-05 16:57:31 +0900

    [34기 김인태] Add: 소셜로그인 기능 구현 완료

---
+++ src/pages/UserSign/Callback.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import qs from "qs";
+
+const Callback = () => {
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = "http://localhost:3000/callback";
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+  const code = new URL(window.location.href).searchParams.get("code");
+  const navigate = useNavigate();
+
+  const getKaKaoToken = async () => {
+    const payload = qs.stringify({
+      grant_type: "authorization_code",
+      client_id: REST_API_KEY,
+      redirect_uri: REDIRECT_URI,
+      code: code,
+      client_secret: CLIENT_SECRET,
+    });
+
+    await axios
+      .post("https://kauth.kakao.com/oauth/token", payload)
+      .then(res => {
+        localStorage.setItem("kakaotoken", res.data.access_token);
+      });
+
+    const kakaoToken = localStorage.getItem("kakaotoken");
+    await axios
+      .get("http://15.164.213.245:8000/users/signin", {
+        headers: { Authorization: kakaoToken },
+      })
+      .then(res => {
+        localStorage.setItem("Authorization", res.config.headers.Authorization);
+        localStorage.removeItem("kakaotoken");
+        navigate("/");
+      });
+  };
+
+  useEffect(() => {
+    getKaKaoToken();
+  }, []);
+
+  return null;
+};
+export default Callback;
--- src/pages/UserSign/UserSign.js
+++ src/pages/UserSign/UserSign.js
@@ -2,7 +2,87 @@ import React from "react";
 import styled from "styled-components";
 
 const Usersign = () => {
-  return <div>UserSign</div>;
+  const clickToKakao = () => {
+    window.location.replace(`${KAKAO_AUTH_URL}`);
+  };
+
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = "http://localhost:3000/callback";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+
+  return (
+    <LoginContainer>
+      <LoginMain>
+        <Title>👋</Title>
+        <Welcome>Welcome!</Welcome>
+        <SubTitle>여행의 모든 것, 마이페이크트립</SubTitle>
+        <KakaoLogin
+          src="/images/UserSign/kakao_login_medium_wide.png"
+          onClick={clickToKakao}
+        />
+        <AnotherLogin>
+          <AnotherLoginButton>페이스북</AnotherLoginButton>
+          <AnotherLoginButton>네이버</AnotherLoginButton>
+          <AnotherLoginButton>이메일</AnotherLoginButton>
+        </AnotherLogin>
+        <NotYetUser>
+          아직 회원이 아니신가요? <SignUpLink>회원가입</SignUpLink>
+        </NotYetUser>
+      </LoginMain>
+    </LoginContainer>
+  );
 };
 
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const LoginMain = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 500px;
+  height: 500px;
+  border: 1px solid #f1f3f5;
+`;
+
+const Title = styled.div`
+  font-size: 60px;
+  margin: 75px;
+`;
+
+const SubTitle = styled.div`
+  font-size: 25px;
+  color: #b2b5b9;
+  margin-top: 15px;
+`;
+const Welcome = styled.div`
+  font-size: 35px;
+`;
+const AnotherLogin = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  margin-top: 25px;
+  font-size: 20px;
+`;
+
+const KakaoLogin = styled.img`
+  margin: 20px;
+  cursor: pointer;
+`;
+const AnotherLoginButton = styled.div`
+  margin-bottom: 30px;
+`;
+
+const NotYetUser = styled.div``;
+
+const SignUpLink = styled.a`
+  text-decoration: underline;
+`;
 export default Usersign;
